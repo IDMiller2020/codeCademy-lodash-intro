33a79bcda30f44c31d5bc9cdb1cdbc338ba98001
Scott <76458930+IDMiller2020@users.noreply.github.com>
2021-06-30 19:00:55 -0600

    ideate clamp function

--- _.js
+++ _.js
@@ -1,6 +1,21 @@
+const _ = {}
 
+// Ideate .clamp():
+function clamp(num) {
+  let clamped
+  // To clamp number by lower bound, say 1 for examle:
+  clamped = Math.max(1, num)
+  // To clamp number by upper bound, say 100 for example:
+  clamped = Math.min(100, clamped)
+  return clamped
+}
+console.log('Numbers clamped between 1 and 100.')
+console.log('Test numbers: -10, 25, 500')
+console.log(clamp(-10))
+console.log(clamp(25))
+console.log(clamp(500))
 
-
+// Implement .clamp():
 
 
 // Do not write or modify code below this line.
