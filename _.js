b0da48fc3863b2e4e080f0ad36ef4820f67de62e
Scott <76458930+IDMiller2020@users.noreply.github.com>
2021-07-01 17:41:21 -0600

    clamp method Implement completed

--- _.js
+++ _.js
@@ -1,4 +1,12 @@
-const _ = {}
+const _ = {
+  // Implement .clamp() method:
+  // .clamp() method called using _.clamp()
+  clamp (number, lower, upper) {
+    const lowerClampedValue = Math.max(number, lower)
+    let clampedValue = Math.min(lowerClampedValue, upper)
+    return clampedValue
+  }
+}
 
 // Ideate .clamp():
 function clamp(num) {
@@ -15,7 +23,11 @@ console.log(clamp(-10))
 console.log(clamp(25))
 console.log(clamp(500))
 
-// Implement .clamp():
+console.log("Numbers clamped between 1 and 100 using _.clamp method.")
+console.log("Test numbers: -10, 25, 500")
+console.log(_.clamp(-10, 1, 100))
+console.log(_.clamp(25, 1, 100))
+console.log(_.clamp(500, 1, 100))
 
 
 // Do not write or modify code below this line.
