cb2c1178df84f8bdff2e6e625e88ac10af8f1d35
Scott <76458930+IDMiller2020@users.noreply.github.com>
2021-07-06 12:34:54 -0600

    inRange Ideate completed

--- _.js
+++ _.js
@@ -5,6 +5,29 @@ const _ = {
     const lowerClampedValue = Math.max(number, lower)
     let clampedValue = Math.min(lowerClampedValue, upper)
     return clampedValue
+  },
+  // Ideate .inRange() method:
+  inRangeIdea (num, start, end) {
+    let startRange
+    let endRange
+    if (!end) {
+      startRange = 0
+      endRange = start
+    } else if (start > end) {
+      startRange = end
+      endRange = start
+    } else {
+      startRange = start
+      endRange = end
+    }
+    return this.checkRangeIdea(num, startRange, endRange)
+  },
+  checkRangeIdea (num, start, end) {
+    if (num >= start && num <= end){
+      return true
+    } else {
+      return false
+    }
   }
 }
 
@@ -28,6 +51,18 @@ console.log("Test numbers: -10, 25, 500")
 console.log(_.clamp(-10, 1, 100))
 console.log(_.clamp(25, 1, 100))
 console.log(_.clamp(500, 1, 100))
+
+// console.log tests for inRange Ideate:
+console.log("_.inRangeIdea(5, 10) should return true:")
+console.log(_.inRangeIdea(5,10))
+console.log("_.inRangeIdea(10, 5) should return false:")
+console.log(_.inRangeIdea(10,5))
+console.log("_.inRangeIdea(10, 5, 20) should return true:")
+console.log(_.inRangeIdea(10,5,20))
+console.log("_.inRangeIdea(30, 5, 20) should return false:")
+console.log(_.inRangeIdea(30,5,20))
+console.log("_.inRangeIdea(1, 5, 20) should return false:")
+console.log(_.inRangeIdea(1,5,20))
 
 
 // Do not write or modify code below this line.
